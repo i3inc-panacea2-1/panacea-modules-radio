a246eef9deba2af0bbe84568945ed95f0bbad629
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:10:05 +0300

    nuget update + font sizes

---
+++ src/Panacea.Modules.Radio/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.Meditech
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
